18abbe129d25f7ebce6d341efae32861611f8d62
Vadim Penkov <raspik92@gmail.com>
2021-09-22 17:47:52 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,48 @@
+from flask import Flask
+from flask import request
+from flask import jsonify
+import requests
+import json
+app = Flask(__name__)
+#TODO
+
+URL = 'https://api.telegram.org/bot1953357082:AAHPqtBfi2HA_IsJrFQDM4m5zkpA6HsnoQ8/'
+
+
+def write_json(data, filename = "answer.json"):
+    with open(filename, "w") as f:
+        json.dump(data,f, indent=2,ensure_ascii=False)
+
+
+def get_updates():
+    url = URL + 'getUpdates'
+    r = requests.get(url)
+    #write_json(r.json())
+    return r.json()
+def send_message(chat_id,text='bla-bla'):
+    url=URL+ 'sendMessage'
+    answer = {"chat_id":chat_id, 'text':text}
+    r = requests.post(url,json=answer)
+    return r.json()
+
+@app.route('/', methods = ['POST','GET'])
+def index():
+    if request.method == "POST":
+        r = request.get_json()
+        write_json(r)
+        return jsonify(r)
+
+def main():
+    # r = requests.get(URL+ "getMe")
+   # write_json(r.json())
+  #  print(r.json())
+    #get_updates()
+ #   r=get_updates()
+  #  chat_id = r["result"][-1]["message"]["chat"]["id"]
+   # print([chat_id])
+   # send_message(chat_id)
+   pass
+
+if __name__ == '__main__':
+   # main()
+    app.run()
