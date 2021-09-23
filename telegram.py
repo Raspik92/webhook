8ebe2a72742bad343d612f5780009631236d53c6
raspik92@gmail.com <Penkov92!>
2021-09-23 18:37:37 +0300

    add method3

--- telegram.py
+++ telegram.py
@@ -2,12 +2,15 @@ import access
 import os
 import telebot
 print("Starting bot")
-bot=telebot.TeleBot(access.api_key)
+bot = telebot.TeleBot(access.api_key)
+
 @bot.message_handler(commands=['Greet'])
 def Greet(message):
     bot.reply_to(message, "HEY pdr")
+
 @bot.message_handler(commands=['Hey'])
 def Hey(message):
     bot.send_message(message.chat.id, "HEY ")
+    bot.send_message(message.chat.id, "HEY2 ")
 
 bot.polling()
